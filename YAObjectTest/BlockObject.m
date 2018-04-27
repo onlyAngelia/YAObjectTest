bd21600f3db923187a2f5db4703e00d7e0c8b016
yxy0720 <479397375@qq.com>
2018-04-27 23:46:30 +0800

    change for testBlockSubstance

--- YAObjectTest/BlockObject.m
+++ YAObjectTest/BlockObject.m
@@ -9,8 +9,7 @@
 #import "BlockObject.h"
 #import <objc/runtime.h>
 
-void(^globalBlock)(void) =^{};
-typedef void(^defineGlobalBlock)(int);
+void(^globalBlock)(void);
 
 @interface BlockObject()
 
@@ -18,42 +17,50 @@ typedef void(^defineGlobalBlock)(int);
 
 @end
 @implementation BlockObject
-+ (void)load
-{
-    Class class = self.class;
-}
 - (instancetype)init
 {
     self = [super init];
     if (self) {
-        //1.第一步探究Block实质，这里仅仅是为了测试方便，正式代码中最好不要在init中直接写大量代码
-//        void (^lockBlock)(void) = ^{
-//
-//        };
-//        lockBlock();
+        
         [self blockTestAllkindsofCase];
-
         
     }
     return self;
 }
 - (void)blockTestAllkindsofCase
 {
-    //globalBlockTest
-    //1.globalBlock 调用时候不引入外部变量
-//    defineGlobalBlock block = ^(){
+    
+    [self testBlocksubstance];//1.第一步探究Block实质从init
+    [self testBlockKinds];//2.探究block类型
+  
+    
+//    NSInteger blockOut = 1;
 //
-//    };
+
+}
+- (void)testBlocksubstance
+{
+    void (^lockBlock)(void) = ^{
+        
+    };
+    lockBlock();
+}
+- (void)testBlockKinds
+{
+      // 调用时候不引入外部变量
+    globalBlock = ^(){
+        
+    };
     _testBlock = ^(){
         
     };
-    NSInteger blockOut = 1;
-   
-    defineGlobalBlock block = ^(int count){
-       
-    };
+    
+}
+- (void)testBlockAutomaticInterceptVar
+{
     __block int a = 0;
     void (^lockBlock)(void) = ^{
+        a++;
     };
     lockBlock();
 }
