01eb161e6c5a2134d6e09ad34588ed1952f9c9ec
yxy0720 <479397375@qq.com>
2018-05-06 01:37:12 +0800

    add C++

---
+++ YAObjectTest/BlockIncludedInOC.cpp
@@ -0,0 +1,19 @@
+//
+//  BlockIncludedInOC.cpp
+//  YAObjectTest
+//
+//  Created by lanjiying on 2018/5/6.
+//  Copyright © 2018年 lanjiying_Anna. All rights reserved.
+//
+
+#include "BlockIncludedInOC.hpp"
+#include <iostream>
+void BlockIncludedInOC::testAutoBlock(){
+    
+}
+void BlockIncludedInOC::testFinalizingBlock(){
+    
+}
+void BlockIncludedInOC::testWeakBlock(){
+    
+}
---
+++ YAObjectTest/BlockIncludedInOC.hpp
@@ -0,0 +1,20 @@
+//
+//  BlockIncludedInOC.hpp
+//  YAObjectTest
+//
+//  Created by lanjiying on 2018/5/6.
+//  Copyright © 2018年 lanjiying_Anna. All rights reserved.
+//
+
+#ifndef BlockIncludedInOC_hpp
+#define BlockIncludedInOC_hpp
+
+#include <stdio.h>
+class BlockIncludedInOC
+{
+public:
+    void testAutoBlock();
+    void testFinalizingBlock();
+    void testWeakBlock();
+};
+#endif /* BlockIncludedInOC_hpp */
